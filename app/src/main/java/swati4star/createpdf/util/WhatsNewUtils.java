04b77d7a96cba301e961f8964cfb9856e9a8bab8
Patrícia da Silva Ramos  Jujuba <56210981+PatriciaRamosS@users.noreply.github.com>
2024-02-09 16:45:48 -0300

    Update WhatsNewUtils.java
    Corrigindo o bug e garantindo que o valor retornado pela chamada do método read() seja verificado

--- app/src/main/java/swati4star/createpdf/util/WhatsNewUtils.java
+++ app/src/main/java/swati4star/createpdf/util/WhatsNewUtils.java
@@ -1,108 +1,87 @@
-package swati4star.createpdf.util;
+package pacoteswati4star.createpdf.util;
 
 import android.app.Dialog;
 import android.content.Context;
 import android.widget.Button;
 import android.widget.TextView;
-
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-
+import java.util.List;
 import swati4star.createpdf.R;
-import swati4star.createpdf.adapter.WhatsNewAdapter;
-import swati4star.createpdf.model.WhatsNew;
+import swati4star.createpdf.adapter.NovidadesAdapter;
+import swati4star.createpdf.model.OQueHaDeNovo;
 
-public class WhatsNewUtils {
+public class NovidadesUtil {
 
-    public static WhatsNewUtils getInstance() {
-        return WhatsNewUtils.SingletonHolder.INSTANCE;
+    private static NovidadesUtil instancia;
+
+    public static NovidadesUtil getInstancia() {
+        if (instancia == null) {
+            instancia = new NovidadesUtil();
+        }
+        return instancia;
     }
 
-    /**
-     * Display dialog with whats new
-     *
-     * @param context - current context
-     */
-    public void displayDialog(Context context) {
-        final Dialog dialog = new Dialog(context);
-        dialog.setContentView(R.layout.fragment_whats_new);
-        RecyclerView rv = dialog.findViewById(R.id.whatsNewListView);
-        TextView title = dialog.findViewById(R.id.title);
-        Button continueButton = dialog.findViewById(R.id.continueButton);
-        continueButton.setText(R.string.whatsnew_continue);
-        title.setText(R.string.whatsnew_title);
+    public void exibirDialog(Context contexto) {
+        final Dialog dialogo = new Dialog(contexto);
+        dialogo.setContentView(R.layout.fragment_whats_new);
+        RecyclerView recyclerView = dialogo.findViewById(R.id.whatsNewListView);
+        TextView titulo = dialogo.findViewById(R.id.title);
+        Button continuarButton = dialogo.findViewById(R.id.continueButton);
+        continuarButton.setText(R.string.whatsnew_continue);
+        titulo.setText(R.string.whatsnew_title);
         try {
-
-            JSONObject obj = new JSONObject(WhatsNewUtils.getInstance().loadJSONFromAsset(context));
-            WhatsNewAdapter whatsNewAdapter = new WhatsNewAdapter(context, extractItemsFromJSON(obj));
-            LinearLayoutManager layoutManager = new LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false);
-            rv.setLayoutManager(layoutManager);
-            rv.setAdapter(whatsNewAdapter);
-            dialog.show();
-            continueButton.setOnClickListener(view -> dialog.dismiss());
-
+            JSONObject obj = new JSONObject(carregarJSONDoAsset(contexto));
+            NovidadesAdapter novidadesAdapter = new NovidadesAdapter(contexto, extrairItensDoJSON(obj));
+            LinearLayoutManager layoutManager = new LinearLayoutManager(contexto, LinearLayoutManager.VERTICAL, false);
+            recyclerView.setLayoutManager(layoutManager);
+            recyclerView.setAdapter(novidadesAdapter);
+            dialogo.show();
+            continuarButton.setOnClickListener(view -> dialogo.dismiss());
         } catch (JSONException e) {
             e.printStackTrace();
         }
     }
 
-    /**
-     * Load data from json file
-     *
-     * @param context - current context
-     * @return - json
-     */
-    private String loadJSONFromAsset(Context context) {
-        String json;
+    private String carregarJSONDoAsset(Context contexto) {
+        String json = null;
         try {
-            InputStream is = context.getAssets().open("whatsnew.json");
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-            json = new String(buffer, StandardCharsets.UTF_8);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            return null;
+            InputStream inputStream = contexto.getAssets().open("whatsnew.json");
+            int tamanho = inputStream.available();
+            byte[] buffer = new byte[tamanho];
+            int bytesLidos = inputStream.read(buffer);
+            if (bytesLidos != -1) {
+                json = new String(buffer, 0, bytesLidos, StandardCharsets.UTF_8);
+            }
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return json;
     }
 
-    /**
-     * Extract what's new items by parsing json
-     *
-     * @param object - json object to be parsed
-     * @return list of whatsnew items
-     * @throws JSONException - invalid JSON
-     */
-    private ArrayList<WhatsNew> extractItemsFromJSON(JSONObject object) throws JSONException {
-
-        ArrayList<WhatsNew> whatsNewList;
-        JSONArray data = object.getJSONArray("data");
-        whatsNewList = new ArrayList<>();
-
-        for (int i = 0; i < data.length(); i++) {
-            JSONObject jsonObject = data.getJSONObject(i);
-            String newTitle = jsonObject.getString("title");
-            String newContent = jsonObject.getString("content");
-            String iconLocation = jsonObject.getString("icon");
-            WhatsNew whatsNew = new WhatsNew(newTitle, newContent, iconLocation);
-            whatsNewList.add(whatsNew);
+    private List<OQueHaDeNovo> extrairItensDoJSON(JSONObject objeto) {
+        List<OQueHaDeNovo> listaNovidades = new ArrayList<>();
+        try {
+            JSONArray dados = objeto.getJSONArray("dados");
+            for (int i = 0; i < dados.length(); i++) {
+                JSONObject jsonObject = dados.getJSONObject(i);
+                String titulo = jsonObject.getString("titulo");
+                String conteudo = jsonObject.getString("conteudo");
+                String iconeLocalizacao = jsonObject.getString("icone");
+                OQueHaDeNovo novidade = new OQueHaDeNovo(titulo, conteudo, iconeLocalizacao);
+                listaNovidades.add(novidade);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
-
-        return whatsNewList;
-    }
-
-    private static class SingletonHolder {
-        static final WhatsNewUtils INSTANCE = new WhatsNewUtils();
+        return listaNovidades;
     }
 }
